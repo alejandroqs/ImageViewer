ce73750a4e435f520e93d1857cf241070fe94452
Alejandro <Alejandro@Alejandro-PC>
2015-01-18 20:48:30 +0000

    First commit

---
+++ src/model/ImagesOnFolder.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.LinkedList;
+
+public class ImagesOnFolder {
+    LinkedList currentImage;
+
+    public ImagesOnFolder(LinkedList currentImage) {
+        this.currentImage = currentImage;
+    }
+}
---
+++ src/model/image.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.io.File;
+
+public class image {
+    File imageFile;
+    File imageNext;
+    File imagePrev;
+    // Cambiar por clase con lista encadenada de ficheros
+}
